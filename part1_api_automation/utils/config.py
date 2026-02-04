f825a29162ae16bb9fd067e0d851956e5da4c167
cdk0825 <60867389+cdk0825@users.noreply.github.com>
2026-02-04 15:22:34 +0900

    pytest 설정파일 추가

---
+++ part1_api_automation/utils/config.py
@@ -0,0 +1,11 @@
+#-----
+# API 기본 설정
+#-----
+BASE_URL = "https://qatrack.elice.io"
+CLASSROOM_ID = "a6bd98a3-83ff-4e5d-ba9e-6c04c69592fc"
+
+# -----
+# 기타 설정
+# -----
+TIMEOUT = 10  # API 요청 타임아웃 (초)
+RETRY = 3     # 실패 시 재시도 횟수
